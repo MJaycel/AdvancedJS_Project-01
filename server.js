ca5512590878916d968a883baed80e52900bf3ab
MJaycel <73226077+MJaycel@users.noreply.github.com>
2022-11-03 12:08:25 +0000

    imported routes index.js and middleware

--- server.js
+++ server.js
@@ -1,38 +1,18 @@
 const express = require('express');
+const { verifyToken } = require('./middleware/validation.js');
+const router = require('./routes/index.js')
 const app = express();
 const port = 9000;
-const jwt = require("jsonwebtoken");
 
 require('dotenv').config()
-
 require('./utils/db.js')();
+require('./middleware/validation.js');
 
 app.use(express.json());
 
 app.use(express.static('public'));
-
-app.use((req, res, next) => {
-    console.log("Time: ", Date.now());
-    res.setHeader('test', "Hello World");
-    next();
-});
-// verify Token
-app.use((req,res,next)=> {
-    if(req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] ==='Bearer'){
-        jwt.verify(req.headers.authorization.split(' ')[1], process.env.APP_KEY, (err,decode) => {
-            if(err)
-                req.user = undefined
-                req.user = decode
-
-            next()
-        })
-    } else{
-        req.user = undefined
-        next()
-    }
-})
-
-app.use('/api/users', require('./routes/users'));
+app.use(verifyToken)
+app.use(router)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
