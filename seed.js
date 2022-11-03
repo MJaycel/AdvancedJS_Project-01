173dfeff5d22a1d45da6fe80fe1af7ee7d91ffce
MJaycel <73226077+MJaycel@users.noreply.github.com>
2022-11-03 19:37:56 +0000

    Database seeding

--- seed.js
+++ seed.js
@@ -1,22 +1,36 @@
 // https://www.mongodb.com/developer/products/mongodb/seed-database-with-fake-data/
 // https://www.mongodb.com/community/forums/t/seeding-mongodb-nodejs/4038
-// const faker = require("faker");
-// const connect = require('./utils/db.js')();
+const {faker} = require('@faker-js/faker');
+require('dotenv').config()
+const connect = require('./utils/db.js');
+const Job = require('./models/jobs_schema.js');
 
-// const User = require('../model/user_schema');
 
-// const numOfUsers = 5;
+const numOfJobs = 10;
+const numOfPara = Math.floor(Math.random() * 8) + 4;
+async function seedDb() {
+    try {
+        await connect();
 
-// try {
-//     await connect()
+        Job.deleteMany();
 
-//     User.drop()
+        for (let i = 0; i < numOfJobs; i++) {
+            // const fullName = faker.name.fullName();
 
-//     for (let i = 0; i < numOfUsers; i++) {
-//         const fullName = faker.name.fullName();
+            const job = new Job();
+            job.jobTitle = faker.name.jobTitle();
+            job.jobArea = faker.name.jobArea();
+            job.location = `${faker.address.county()}, ${faker.address.country()} `;
+            job.salary = faker.finance.amount(10, 25, 2, '€');
+            job.roleDesc = faker.lorem.paragraphs(numOfPara);
+            job.responsibilites = faker.lorem.sentence(Math.floor(Math.random() * 10) + 2)
+            job.qualifications = faker.lorem.sentence(Math.floor(Math.random() * 10) + 2)
+            job.save()
+        }
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
 
-//     }
-// }
-// catch (err) {
-//     console.log(err);
-// }
+seedDb();
